571f90b0b5ed25ac37818e5676d1e9042f9cd29e
Sean McGrath <sean@mcgrath.net.nz>
2022-03-08 17:18:45 +1300

    Update config file with postgres connection details

---
+++ config.js
@@ -0,0 +1,54 @@
+module.exports = {
+	//address and port to which server will bind, host can also be a hostname
+	//by default it will listen on all ipv4 and ipv6 addresses on port 7777
+	//"host": "127.0.0.1",
+	//"port": 7777,
+
+	//length of random characters in link that's generated on document save
+	"keyLength": 10,
+	//max allowed paste length - 0 for unlimited
+	"maxLength": 400000,
+
+	//algorithm used to generate random characters
+	//see docs/generators.md for more information
+	"keyGenerator": {
+		"type": "phonetic"
+	},
+
+	//max age for static website assets
+	"staticMaxAge": 60 * 60 * 24,
+
+	//TODO: re-add more options to logging
+	//logging preferences
+	"logging": {
+		//can be one of: error, warn, info, http, verbose, debug, silly
+		"level": "info"
+	},
+
+	//rate limits for requests, can be omitted
+	//handled by express-rate-limit, options can be found here: https://github.com/nfriedly/express-rate-limit/blob/master/lib/express-rate-limit.js#L7-L14
+	"rateLimits": {
+		"windowMs": 30 * 60 * 1000,
+		"max": 250
+	},
+
+	//storage system used for storing saved haste documents
+	//see docs/storage.md for more information
+	"storage": {
+		"type": "postgres",
+		"expire": process.env.PASTE_EXPIRY_SECONDS || 5184000,
+		"clientOptions": {
+			"host": process.env.POSTGRES_HOST || "localhost",
+			"port": process.env.POSTGRES_PORT || 5432,
+			"user": process.env.POSTGRES_USER || "haste",
+			"password": process.env.POSTGRES_PASS || "haste",
+			"database": process.env.POSTGRES_DB || "haste",
+			"ssl": process.env.POSTGRES_USESSL || true
+		}
+	},
+
+	//static documents that will never expire ("name": "path")
+	"documents": {
+		"about": "./about.md"
+	}
+};
